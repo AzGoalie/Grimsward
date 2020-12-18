b542128e6d359ccf43f9c8907e7dde0332157e54
Travis McMahon <azgoalie@gmail.com>
2020-12-18 01:36:57 -0700

    Initial navbar

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,7 +1,14 @@
 (ns app.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
+            [re-frame.core :as rf]
+            [app.db]
             [app.theme :refer [grimsward-theme]]
+            ;; -- nav --
+            [app.nav.views :refer [nav]]
+            [app.nav.events]
+            [app.nav.subs]
+            ;; -- npm deps --
             ["@material-ui/core" :as mui]
             ["@material-ui/core/styles" :refer [ThemeProvider]]))
 
@@ -9,10 +16,11 @@
   []
   [:> ThemeProvider {:theme grimsward-theme}
    [:> mui/CssBaseline]
-   [:> mui/Typography "Grimsward"]])
+   [nav]])
 
 (defn start
   []
+  (rf/dispatch-sync [:initialize-db])
   (rdom/render [app]
                (.getElementById js/document "app")))
 
---
+++ src/app/nav/views.cljs
@@ -0,0 +1,53 @@
+(ns app.nav.views
+  (:require [re-frame.core :as rf]
+            ["@material-ui/core" :as mui]
+            ["@material-ui/icons" :as icons]))
+
+(defn nav-item
+  [{:keys [id href dispatch name active-nav]}]
+  (:> mui/Typography
+      [:> mui/Link {:href href
+                    :on-click dispatch
+                    :color "inherit"
+                    :style {:padding 10}
+                    :underline (if (= id active-nav) "always" "hover")}
+       name]))
+
+(defn nav-public
+  [{:keys [active-nav]}]
+  [:<>
+   [nav-item {:id :sign-up
+              :name "Sign Up"
+              :href "#signup"
+              :active-nav active-nav
+              :dispatch #(rf/dispatch [:set-active-nav :sign-up])}]
+   [nav-item {:id :log-in
+              :name "Log In"
+              :href "#login"
+              :active-nav active-nav
+              :dispatch #(rf/dispatch [:set-active-nav :log-in])}]])
+
+(defn nav-authenticated
+  [{:keys [active-nav]}]
+  [:<>
+   [nav-item {:id :campaigns
+              :name "Campaigns"
+              :href "#campaigns"
+              :active-nav active-nav
+              :dispatch #(rf/dispatch [:set-active-nav :campaigns])}]
+   [nav-item {:id :profile
+              :name "[Account Name Here]"
+              :href "#profile"
+              :active-nav active-nav
+              :dispatch #(rf/dispatch [:set-active-nav :profile])}]])
+
+(defn nav
+  []
+  (let [active-nav @(rf/subscribe [:active-nav])]
+    [:> mui/AppBar {:position "static"}
+     [:> mui/Toolbar
+      [:> mui/IconButton {:edge "start" :color "inherit"}
+       [:> icons/Menu]]
+      [:> mui/Typography {:variant "h6" :style {:flexGrow 1}}
+       "Grimsward"]
+      [nav-public {:active-nav active-nav}]]]))
