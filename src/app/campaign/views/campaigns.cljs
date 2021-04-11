8daf790ca2e87b61afadb3f67aff6d1c03458d2d
Travis McMahon <azgoalie@gmail.com>
2021-04-11 00:27:20 -0700

    Swapped to Container from Flex Fixed input jumping Moved re-frame-10x to dev mode

--- src/app/campaign/views/campaigns.cljs
+++ src/app/campaign/views/campaigns.cljs
@@ -1,18 +1,18 @@
 (ns app.campaign.views.campaigns
   (:require [re-frame.core :as rf]
             [app.campaign.views.campaign-card :refer [campaign-card]]
-            ["@chakra-ui/react" :refer [Button Center Flex Heading Stack]]))
+            ["@chakra-ui/react" :refer [Button Center Container Heading Stack]]))
 
 (defn campaigns
   []
   (let [campaigns (rf/subscribe [:campaigns])]
-    [:> Flex {:align   "center"
-              :justify "center"}
+    [:> Container {:center-content true}
      [:> Stack {:spacing 8}
       [:> Heading {:align "center"}
        "Campaigns"]
       (for [campaign @campaigns]
         ^{:key (:id campaign)}
         [campaign-card campaign])
-      [:> Button {:full-width true}
+      [:> Button {:size "lg"
+                  :is-full-width true}
        "Create a new campaign"]]]))
--- src/app/components/form_group.cljs
+++ src/app/components/form_group.cljs
@@ -1,12 +1,19 @@
 (ns app.components.form-group
-  (:require ["@chakra-ui/react" :refer [FormControl FormErrorMessage FormLabel Input]]))
+  (:require [reagent.core :as r]
+            ["@chakra-ui/react" :refer [FormControl FormErrorMessage FormLabel Input]]))
+
+(def r-input
+  (r/reactify-component
+   (fn [props]
+     [:input props])))
 
 (defn form-group
   [{:keys [id label type values placeholder error]}]
   [:> FormControl {:id         id
                    :is-invalid error}
    [:> FormLabel label]
-   [:> Input {:id          id
+   [:> Input {:as          r-input
+              :id          id
               :type        type
               :value       (id @values)
               :on-change   #(swap! values assoc id (.. % -target -value))
